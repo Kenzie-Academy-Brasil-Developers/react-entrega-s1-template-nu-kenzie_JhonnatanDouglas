12f9d1f7db7a95e13376977b72fbe67fb75ec5d5
JhonnatanDouglas <jhonnatanaraujoDEV@gmail.com>
2023-06-02 18:35:29 -0300

    Ver3 Entrega: Nu Kenzie - Corrigido o Problema da Logo no Repositorio do Vercel

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,13 @@
 import { StyledResponsiveDivHeader } from '../../styles/grid'
 import { StyledHeader } from './style'
+import logo from '../../assets/Nu-Kenzie-logo.svg'
 
 export const HeaderDefault = () => {
   return (
     <StyledHeader>
       <StyledResponsiveDivHeader>
         <figure>
-          <img src='./src/assets/Nu-Kenzie-logo.svg' alt='Logo da Nu Kenzie' />
+          <img src={ logo } alt='Logo da Nu Kenzie' />
         </figure>
       </StyledResponsiveDivHeader>
     </StyledHeader>
